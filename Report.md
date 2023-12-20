5f44917796df557a88d6bc8bba1edee9abb09277
Lorena Raichle <Lorena@MBP-Lorena.yallo.box>
2023-12-20 18:25:53 +0100

    Assignment 6

--- Report.md
+++ Report.md
@@ -1,41 +1,62 @@
-Assignment 6
+Assignment 6 Solution
 ---------------------
 
 # Team Members
 
-- Sanjiv Jha
-- Jérémy Lemée
-- Andrei Ciortea
+- Joey Lau
+- Lorena Raichle
 
 # GitHub link to your (forked) repository
 
-...
 
 # Task 1
 
 1. WebIDs of the group members
 
 Ans:
+https://solid.interactions.ics.unisg.ch/LorenaRaichle/profile/card#me
+https://solid.interactions.ics.unisg.ch/Joey-Lau/profile/card#me
 
 
 2. Group profile
 
-Ans:
-
-
+Ans: https://solid.interactions.unisg.ch/LorenaRaichle/TeamJoeyLorena
 
 
 # Task 2
 
 1. What command did you perform to get the group name from the WebId?
 
-Ans: 
+Ans:
+
+'SELECT ?group WHERE { ?group foaf:member <https://solid.interactions.ics.unisg.ch/TeamJoeyLorena/profile/card#me> . }'
+
 
 
 2. Which command did you perform to get the group members from the WebId?
 
 Ans:
 
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+SELECT ?memberName
+WHERE {
+<https://solid.interactions.ics.unisg.ch/TeamJoeyLorena/sample-team> foaf:member ?member .
+?member foaf:name ?memberName .
+}
+
+
+[
+{"memberName":"\"LorenaRaichle\""}
+]
+
+
+SELECT ?memberName WHERE { ?s ?p ?memberName }
+
+[
+{"memberName":"http://xmlns.com/foaf/0.1/Group"},
+{"memberName":"https://solid.interactions.ics.unisg.ch/LorenaRaichle/profile/card#me"},
+{"memberName":"https://solid.interactions.ics.unisg.ch/Joey-Lau/profile/card#me"},
+]
 
 
 3. Which command did you performed to get the group members from the WebId without link traversal? Which result did you get? Is it correct?
---
+++ Task3/app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const cors = require('cors');
+const app = express();
+const http = require('http');
+const path = require('path');
+const httpServer = http.Server(app);
+const {Server} = require("socket.io");
+const io = new Server(httpServer);
+
+const QueryEngine = require('@comunica/query-sparql').QueryEngine;
+
+const engine = new QueryEngine();
+
+const port = 5001;
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/movies', (req, res) => {
+    let input_url = req.query.url
+    console.log("input url: ", input_url)
+    res.statusCode = 200
+    res.setHeader("Content-Type", "text/html");
+    res.sendFile(path.join(__dirname, 'index.html'));
+    handle_query(input_url)
+});
+
+async function handle_query(input_url) {
+    let movie_urls = [] //List of movies in the pod
+    engine.queryBindings(`SELECT ?v WHERE {
+      ?movie a <http://www.w3.org/ns/ldp#BasicContainer> ;
+          <http://www.w3.org/ns/ldp#contains> ?v. 
+      }`, {
+        sources: [input_url],
+    }).then(function (bindingsStream) {
+        bindingsStream.on('data', function (data) {
+            movie_urls.push(data.get('v').value) //Variable ?v in the SPARQL query.
+        });
+        bindingsStream.on('end', function () {
+            console.log(movie_urls)
+            engine.queryBindings(`SELECT ?name ?image WHERE {?movie <https://schema.org/name> ?name.
+?movie <https://schema.org/image> ?image.} `, {
+                sources: movie_urls,
+            }).then(function (bindingsStream) {
+                bindingsStream.on('data', function (data) {
+                    let obj = {
+                        "name": data.get('name').value, //Variable ?name in the SPARQL query.
+                        "image": data.get('image').value //Variable ?image in the SPARQL query.
+                    };
+                    io.emit('update', {'message': obj}) //Send information to the browser.
+                });
+            });
+        })
+
+    });
+}
+
+
+httpServer.listen(port),
+    () => console.log("Server is running... on " + port);
